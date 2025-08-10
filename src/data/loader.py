e032860e1ed3629e3f37551ffa0faf4a28904b80
anujdutt9 <anuj.dutt9@gmail.com>
2025-08-09 20:54:13 -0700

    swapping answers in hallueval loader

--- src/data/loader.py
+++ src/data/loader.py
@@ -444,8 +444,10 @@ class HalluEvalLoader(DatasetLoader):
         return {
             "id": str(uuid.uuid5(uuid.NAMESPACE_DNS, query)),
             "question": query,
-            "steps": right_answer,
-            "answer": hallucinated_answer,
+            # "steps": right_answer,
+            # "answer": hallucinated_answer,
+            "steps": hallucinated_answer,
+            "answer": right_answer,
         }
 
     def load_dataset(self) -> Dataset:
